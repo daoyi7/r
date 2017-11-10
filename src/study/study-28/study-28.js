c01edd2e8f6dcce2609428ff98569b4338d1d154
daoyi7 <kawhichina@gmail.com>
2017-11-10 22:04:06 +0800

    重新学习study28

--- src/study/study-28/study-28.js
+++ src/study/study-28/study-28.js
@@ -1,27 +1,9 @@
 import React, {Component} from 'react'
 
 export default class App extends Component {
-  state = {
-    name: ['kawhi']
-  }
-
-  checkChange = this.checkChange.bind(this)
-
-  checkChange(e) {
-    if(e.target.checked) {
-      this.setState({
-        name: e.target.value
-      })
-    }
-    console.log(this.state.name)
-  }
-
   render() {
     return (
-      <div>
-        <input type="checkbox" onChange = {this.checkChange} value='kobe' /><span>科比</span><br />
-        <input type="checkbox" onChange = {this.checkChange} value='gay' checked={this.state.name.indexOf('gay') > -1} /><span>盖伊</span>
-      </div>
+      <p>1</p>
     )
   }
 }
