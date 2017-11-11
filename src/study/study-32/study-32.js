058783862ed9d31af1f140b676a9cb8ee28a55a2
daoyi7 <kawhichina@gmail.com>
2017-11-11 21:38:55 +0800

    遇到bug了 - -

---
+++ src/study/study-32/study-32.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react'
+
+export default class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      count: 0
+    }
+  }
+
+  clickFn = this.clickFn.bind(this)
+
+  clickFn() {
+    this.setState({
+      count: 1
+    })
+    this.setState({
+      count: this.state.count + 1
+    })
+    this.setState({
+      count: this.state.count + 2
+    })
+  }
+
+  render() {
+    return (
+      <div>
+          <button onClick={this.clickFn}>{this.state.count}</button>
+      </div>
+    )
+  }
+}
